c7eeb8d8900c78bfadda7ff12078a81d3b91c6d8
Jean Paul <yjeanpaul@gmail.com>
2026-01-09 14:59:30 -0500

    Completed angular forms class about form group

--- Curso de Angular Forms/README.md
+++ Curso de Angular Forms/README.md
@@ -2,9 +2,15 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.11.
 
+## Installation
+
+```bash
+$npm install --legacy-peer-deps
+```
+
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `npm run start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
---
+++ Curso de Angular Forms/src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss']
+})
+export class BasicFormComponent implements OnInit {
+
+  // nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
+  // emailField = new FormControl('');
+  // phoneField = new FormControl('');
+  // colorField = new FormControl('#fff');
+  // dateField = new FormControl('');
+  // ageField = new FormControl(12);
+  // categoryField = new FormControl('');
+  // tagField = new FormControl('');
+
+  // agreeField = new FormControl(false);
+  // genderField = new FormControl();
+  // zoneField = new FormControl();
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    color: new FormControl('#fff'),
+    date: new FormControl(''),
+    age: new FormControl(12),
+    category: new FormControl(''),
+    tag: new FormControl(''),
+    agree: new FormControl(false),
+    gender: new FormControl(),
+    zone: new FormControl()
+  })
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+    this.nameField?.valueChanges.subscribe(value => console.log(value))
+  }
+
+  save(event) {
+    console.log(this.form.value)
+  }
+
+  logNameValue () {
+    console.log(this.nameField?.value);
+  }
+
+  isNameFieldValid () {
+    return this.nameField?.touched && this.nameField?.valid;
+  }
+  
+  isNameFieldInvalid () {
+    return this.nameField?.touched && this.nameField?.invalid;
+  }
+  
+  get nameField () {
+    return this.form.get('name');
+  }
+
+  get emailField () {
+    return this.form.get('email');
+  }
+
+  get phoneField () {
+    return this.form.get('phone');
+  }
+
+  get colorField () {
+    return this.form.get('color');
+  }
+
+  get dateField () {
+    return this.form.get('date');
+  }
+
+  get ageField () {
+    return this.form.get('age');
+  }
+
+  get categoryField () {
+    return this.form.get('category');
+  }
+
+  get tagField () {
+    return this.form.get('tag');
+  }
+
+  get agreeField () {
+    return this.form.get('agree');
+  }
+
+  get genderField () {
+    return this.form.get('gender');
+  }
+
+  get zoneField () {
+    return this.form.get('zone');
+  } 
+}
