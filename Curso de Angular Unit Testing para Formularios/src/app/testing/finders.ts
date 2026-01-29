58268500c680aad248acb8a56b7e4171307689cb
Jean Paul <yjeanpaul@gmail.com>
2026-01-29 17:29:08 -0500

    Agregado Curso de Angular Testing para Formularios

---
+++ Curso de Angular Unit Testing para Formularios/src/app/testing/finders.ts
@@ -0,0 +1,29 @@
+import {By} from "@angular/platform-browser";
+import {ComponentFixture} from "@angular/core/testing";
+import {Type} from "@angular/core";
+
+export function getText<T>(fixture: ComponentFixture<T>, testId: string) {
+  const debugElement = queryById(fixture, testId);
+  const element: HTMLElement = debugElement.nativeElement;
+  return element.textContent;
+}
+
+export function query<T>(fixture: ComponentFixture<T>, selector: string) {
+  const debugElement = fixture.debugElement.query(By.css(selector));
+  if (!debugElement) {
+    throw new Error(`query: Element with ${selector} not found`)
+  }
+  return debugElement;
+}
+
+export function queryById<T>(fixture: ComponentFixture<T>, selector: string) {
+  return query(fixture, `[data-testid="${selector}"]`);
+}
+
+export function queryAll<T>(fixture: ComponentFixture<T>, selector: string) {
+  return fixture.debugElement.queryAll(By.css(selector));
+}
+
+export function queryAllByDirective<T, D>(fixture: ComponentFixture<T>, directive: Type<D>) {
+  return fixture.debugElement.queryAll(By.directive(directive));
+}
